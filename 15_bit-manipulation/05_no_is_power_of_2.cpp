f986bb1688ae6a5f4f3afd27534afa31c8c383bb
Mohammad Faizan <mohammadfaiizan@gmail.com>
2023-09-03 00:59:39 +0530

    Update 05_no_is_power_of_2.cpp

--- 15_bit-manipulation/05_no_is_power_of_2.cpp
+++ 15_bit-manipulation/05_no_is_power_of_2.cpp
@@ -31,3 +31,13 @@ bool isPowerofTwo(long long n) {
 }
 
 // ----------------------------------------------------------------------------------------------------------------------- //
+GFG Solution
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+        return (n > 0) && ((n & (n - 1)) == 0);
+    }
+};
