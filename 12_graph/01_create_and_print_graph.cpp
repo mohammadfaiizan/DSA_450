618428a1566beb2e6fc48cdf669a5c7752df7d87
Mohammad Faizan <mohammadfaiizan@gmail.com>
2023-09-03 00:35:53 +0530

    Update 01_create_and_print_graph.cpp

--- 12_graph/01_create_and_print_graph.cpp
+++ 12_graph/01_create_and_print_graph.cpp
@@ -57,3 +57,22 @@ int adjList() {
         //                         adj[v].push_back({u, wt});
     }
 }
+
+
+GFG Solution:
+//---------------------------------------------------------------------------------------------------------------//
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        // Code here
+        vector<vector<int>> adj_list(V);
+        for(const auto& edge: edges){
+            int u = edge.first;
+            int v = edge.second;
+            adj_list[u].push_back(v);
+            adj_list[v].push_back(u);
+        }
+        return adj_list;
+    }
+};
